3702103ea18aec6c4736ddbfa1a348b01c246778
Wase-Zahin <w.zahin@yahoo.com>
2024-05-13 23:58:24 +0800

    feat: hide button for medium screen

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -24,7 +24,7 @@ const Modal: React.FC<ModalProps> = (props) => {
         <>
             <button
                 onClick={toggleModal}
-                className="absolute -left-9999 opacity-0"
+                className="md:hidden absolute -left-9999 opacity-0"
                 tabIndex={-1}
             >
                 Click me
