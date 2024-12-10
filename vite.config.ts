25955829e4a0170c67049c1f87155c3a77a8069a
Wase-Zahin <w.zahin@yahoo.com>
2024-12-11 01:15:52 +0600

    feat: update files

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({ mode}) => {
 
     return {
         plugins: [react()],
+        build: {
+            outDir: 'dist',
+        },
         server: {
             proxy: {
                 '/api': {
