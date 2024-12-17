7480ed6595b6dcf4005625368e3cc8b4c1f1b932
Wase-Zahin <w.zahin@yahoo.com>
2024-12-18 05:14:38 +0600

    feat: update file path using env vars

--- src/Services/typings/Enums.ts
+++ src/Services/typings/Enums.ts
@@ -1,8 +1,8 @@
 const minioURI = import.meta.env.VITE_API_MINIO_BASE;
 
 export const Banner = {
-    Large: `http://${minioURI}/kobutor/2024/photo-1587731556938-38755b4803a6.jpg`,
-    Small: `http://${minioURI}/kobutor/2024/vlad-bunu-d2Q0ZXnWI_c-unsplash.jpg`,
+    Large: `${minioURI}/kobutor/2024/photo-1587731556938-38755b4803a6.jpg`,
+    Small: `${minioURI}/kobutor/2024/vlad-bunu-d2Q0ZXnWI_c-unsplash.jpg`,
 } as const;
 
 export enum Category {
