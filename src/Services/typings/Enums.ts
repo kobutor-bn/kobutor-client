5db4a42455e0007840c8431e8c1a778819f9d276
Wase-Zahin <w.zahin@yahoo.com>
2024-12-18 07:28:48 +0600

    feat: update file path using env vars

--- src/Services/typings/Enums.ts
+++ src/Services/typings/Enums.ts
@@ -1,9 +1,7 @@
-// const minioURI = import.meta.env.VITE_API_MINIO_INCLUDE_BUCKET;
-
 export const Banner = {
-    Large: `http://16.16.18.26:9000/kobutor/2024/photo-1587731556938-38755b4803a6.jpg`,
-    Small: `http://16.16.18.26:9000/kobutor/2024/vlad-bunu-d2Q0ZXnWI_c-unsplash.jpg`,
-} as const;
+    Large: "/kobutor/2024/photo-1587731556938-38755b4803a6.jpg",
+    Small: "/kobutor/2024/vlad-bunu-d2Q0ZXnWI_c-unsplash.jpg",
+};
 
 export enum Category {
     WomenBags = 'Women Bags',
